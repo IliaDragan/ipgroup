bb3afc97fb4e89b0f76878ae40ebd81badcdf723
mak <mak@peytz.dk>
2015-05-06 07:48:41 -0700

    Modify team members block for project node page

--- themes/custom/air/scripts/field_user.js
+++ themes/custom/air/scripts/field_user.js
@@ -4,17 +4,49 @@
  */
 Drupal.behaviors.userField = {
   attach: function (context, settings) {
-    var $user = jQuery('.team-member', context);
-    var $info = jQuery('.team-members-info', context);
-    $user.mouseenter(function() {
-      var $this = jQuery(this);
-      var id = $this.attr('id');
-      $info.removeClass('hidden').find('.' + id).removeClass('hidden');
-    });
-    $user.mouseleave(function() {
-      var $this = jQuery(this);
-      var id = $this.attr('id');
-      $info.addClass('hidden').find('.' + id).addClass('hidden');
+    jQuery(window).load(function(){
+      var $photo = jQuery('.field-name-field-image img', context);
+      var $users = jQuery('.team-members', context);
+      var $title = jQuery('.pane-node-field-user .pane-title', context);
+      var $button = jQuery('.team-members-collapse-expand', context);
+      var original_height = $users.height();
+      var height = $photo.length ? $photo.height() : 0;
+
+      if (height > 0) {
+        height -= $title.height();
+        // Remove paddings.
+        height -= parseInt($title.css('padding-top'));
+        height -= parseInt($title.css('padding-bottom'));
+        height -= parseInt($users.css('padding-top'));
+        height -= parseInt($users.css('padding-bottom'));
+        // Remove borders 2px.
+        height -= 2;
+        $users.css('min-height', height);
+      }
+
+      if (original_height <= height || height == 0) {
+        $button.remove();
+        $users.css('position', 'relative');
+      }
+      else {
+        $users.css('height', height);
+        $button.removeClass('expanded');
+
+        $button.on('click', function () {
+          if ($button.hasClass('expanded')) {
+            $users.animate({
+              height: height
+            }, 1000);
+            $button.removeClass('expanded');
+          }
+          else {
+            $button.addClass('expanded');
+            $users.animate({
+              height: original_height
+            }, 1000);
+          }
+        });
+      }
     });
   }
 }
--- themes/custom/air/templates/base/field--field-user.tpl.php
+++ themes/custom/air/templates/base/field--field-user.tpl.php
@@ -4,10 +4,17 @@
  * Default theme implementation for user refernce field.
  */
 ?>
-
 <div class="team-members">
   <?php foreach ($items as $item): ?>
   <div class="team-member" id="<?php print $item['identifier']; ?>">
+    <div class="team-member-info-left">
+      <?php if (!empty($item['photo'])): ?>
+        <div class="team-member-photo">
+          <?php print $item['photo']; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+    <div class="team-member-info-right">
     <?php if (!empty($item['name_link'])): ?>
     <div class="team-member-field-name">
       <?php print $item['name_link']; ?>
@@ -18,25 +25,17 @@
       <?php print $item['position']; ?>
     </div>
     <?php endif; ?>
-  </div>
-  <?php endforeach; ?>
-  <div class="team-members-info hidden">
-    <?php foreach ($items as $item): ?>
-    <div class="team-member-info  <?php print $item['identifier']; ?> hidden">
-      <?php if (!empty($item['photo'])): ?>
-      <div class="team-member-photo">
-        <?php print $item['photo']; ?>
-      </div>
-      <?php endif; ?>
-      <?php if (!empty($item['phone'])): ?>
+
+    <?php if (!empty($item['phone'])): ?>
       <div class="team-member-phone">
         <?php print $item['phone']; ?>
       </div>
-      <?php endif; ?>
-      <div class="team-member-mail">
-        <?php print $item['#options']['entity']->mail; ?>
-      </div>
+    <?php endif; ?>
+    <div class="team-member-mail">
+      <?php print $item['#options']['entity']->mail; ?>
     </div>
-    <?php endforeach; ?>
+    </div>
   </div>
+  <?php endforeach; ?>
+  <div class="team-members-collapse-expand expanded">&nbsp;</div>
 </div>
